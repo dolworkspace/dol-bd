baed25682767fa8aee242bdb2d5947b7ef485b31
dolworkspace <dolworkspace@gmail.com>
2026-04-28 11:46:30 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -92,7 +92,7 @@ try:
             
             if selected_indices:
                 filtered_gdf = gdf.loc[selected_indices]
-                
+                st.dataframe(filtered_gdf)
                 # แผนที่ภาพถ่ายดาวเทียม
                 m = leafmap.Map(google_map="SATELLITE")
                 m.add_gdf(filtered_gdf, layer_name="Preview")
